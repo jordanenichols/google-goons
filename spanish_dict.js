40647d9c58b9629b9d87c24041ad9679b17f70d6
KevinKByte <22605103+KevinKbyte@users.noreply.github.com>
2019-11-17 02:14:16 -0500

    finished base for filter by spanish word to grab definition list
    Signed-off-by: KevinKByte <22605103+KevinKbyte@users.noreply.github.com>

--- spanish_dict.js
+++ spanish_dict.js
@@ -9,47 +9,54 @@ function evaluateXPath(aNode, aExpr) {
     found.push(res);
   return found;
 }
+     
+var input = "Europa";
 
 var xmlCont = chrome.runtime.getURL("/thirdParty/spa-eng.tei"); //this returns the FULL_PATH of the .xml file [NOT THE XML NODES]
-            console.log(xmlCont) //You can see it here
+// console.log(xmlCont) //You can see it here
 
-                    $.ajax({                    
-                          type: "GET" , 
-                          url: xmlCont,//Place that FULL_PATH here 
-                          dataType: "xml" , //This too is important 
+$.ajax({                    
+      type: "GET" , 
+      url: xmlCont,//Place that FULL_PATH here 
+      dataType: "xml" , //This too is important 
 
-                         success: function(xml) { 
+     success: function(xml) { 
 
-                          console.log(xml)// see the ajax results if your curious
+      console.log(xml)// see the ajax results if your curious
 
-                        //var xmlDoc = $.parseXML( xml );//NOT NEED FOR THIS
+    //var xmlDoc = $.parseXML( xml );//NOT NEED FOR THIS
 
-                        //console.log(xmlDoc);// if you parseXML it returns null
+    //console.log(xmlDoc);// if you parseXML it returns null
 
-                        var entries = $(xml).find('body').find('entry'); //Show the text between the <title> tags
+    var entries = $(xml).find('body').find('entry'); //Show the text between the <title> tags
 
-                         // for (i = 0; i < spanish.length; i++){
-                         //    console.log(spanish[i].textContent)
-                         // }
-                         for (i = 0; i < entries.length; i++){
-                            var entry = entries[i];
-                            var spanish = entry.getElementsByTagName("form")[0].getElementsByTagName("orth")[0].textContent; 
-                            var englishes = entry.getElementsByTagName("sense"); 
-                            var english_list = []
-                             for (j = 0; j < englishes.length; j++) {
-                                 var english = englishes[j].getElementsByTagName("cit")[0].getElementsByTagName("quote")[0].textContent;
-                                 english_list.push(english);
-                             }
+    // var regex = new RegExp("^Europa$");
+    // speedy
+    // $(entries).filter(function () {
+    //     return regex.test($(this).text()); 
+    // });
 
-                             if (spanish == "Europa"){
-                                console.log(spanish + " " + english_list.toString());
-                             }
-                         }
+    // for (i = 0; i < spanish.length; i++){
+    //    console.log(spanish[i].textContent)
+    // }
+    
 
+     for (i = 0; i < entries.length; i++){
+        var entry = entries[i];
+        var spanish = entry.getElementsByTagName("form")[0].getElementsByTagName("orth")[0].textContent; 
+        var englishes = entry.getElementsByTagName("sense"); 
+        var english_list = []
+         for (j = 0; j < englishes.length; j++) {
+             var english = englishes[j].getElementsByTagName("cit")[0].getElementsByTagName("quote")[0].textContent;
+             english_list.push(english);
+         }
 
-						//var entry = title.getElementsByTagName('entry').text(); //Show the text between the <title> tags
-                        console.log(typeof title);
-						
+         if (spanish === input){
+             console.log(english_list);
+         }
+     }
 
-                    }    
-                        });
+    //var entry = title.getElementsByTagName('entry').text(); //Show the text between the <title> tags
+
+}    
+    });
