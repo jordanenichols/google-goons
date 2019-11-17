1d94f5260f6b37a3b8794e1dde0aee3510b9fb01
KevinKByte <22605103+KevinKbyte@users.noreply.github.com>
2019-11-17 01:04:15 -0500

    google tts url in background.js
    Signed-off-by: KevinKByte <22605103+KevinKbyte@users.noreply.github.com>

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+// http://translate.google.com/translate_tts?ie=UTF-8&tl=en&client=tw-ob&q=textyouwannaberead
 chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({color: '#3aa757'}, function() {
       console.log("The color is green.");
