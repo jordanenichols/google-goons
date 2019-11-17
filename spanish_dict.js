36a4a481ee4e596880f57efd9b35eb367fbf34ab
KevinKByte <22605103+KevinKbyte@users.noreply.github.com>
2019-11-16 22:18:51 -0500

    base xml reader
    Signed-off-by: KevinKByte <22605103+KevinKbyte@users.noreply.github.com>

--- spanish_dict.js
+++ spanish_dict.js
@@ -1,17 +1,15 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_using_XPath_in_JavaScript
 
+var req = new XMLHttpRequest();
 
-var json = (function() {
-        var json = null;
-        $.ajax({
-            'async': false,
-            'global': false,
-            'url': "https://freedict.org/freedict-database.json",
-            'dataType': "json",
-            'success': function (data) {
-                json = data;
-            }
-        });
-        return json;
-    })();
-	
+req.open("GET", "chrome://yourextension/content/peopleDB.xml", false); 
+req.send(null);
+
+var xmlDoc = req.responseXML;		
+
+var nsResolver = xmlDoc.createNSResolver( xmlDoc.ownerDocument == null ? xmlDoc.documentElement : xmlDoc.ownerDocument.documentElement);
+
+var personIterator = xmlDoc.evaluate('//person', xmlDoc, nsResolver, XPathResult.ANY_TYPE, null );
+
 console.log(json);
+
