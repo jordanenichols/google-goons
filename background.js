3886afd0c9768687a0a871f7b4c610bc6ef0d688
jordanenichols <jordan.jbns@gmail.com>
2019-11-22 16:33:38 -0500

    idk

--- background.js
+++ background.js
@@ -1,15 +1,7 @@
 // http://translate.google.com/translate_tts?ie=UTF-8&tl=en&client=tw-ob&q=textyouwannaberead
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
-    });
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-      chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'developer.chrome.com'},
-        })
-        ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-      }]);
-    });
-  });
+chrome.browserAction.onClicked.addListener(function (tab) {
+	// for the current tab, inject the "inject.js" file & execute it
+	chrome.tabs.executeScript(tab.ib, {
+		file: 'inject.js'
+	});
+});
--- parser.js
+++ parser.js
@@ -1,6 +1,81 @@
-import {getTranslation} from "./spanish_dict.js";
+function evaluateXPath(aNode, aExpr) {
+    var xpe = new XPathEvaluator();
+    var nsResolver = xpe.createNSResolver(aNode.ownerDocument == null ?
+      aNode.documentElement : aNode.ownerDocument.documentElement);
+    var result = xpe.evaluate(aExpr, aNode, nsResolver, 0, null);
+    var found = [];
+    var res;
+    while (res = result.iterateNext())
+      found.push(res);
+    return found;
+  }
+       
+  
+  
+  var xmlCont = chrome.runtime.getURL("/thirdParty/spa-eng.tei"); //this returns the FULL_PATH of the .xml file [NOT THE XML NODES]
+  // console.log(xmlCont) //You can see it here
+  
+  var input = "INPUT YOUR SHIT HERE YOU APES";
+  
+function getTranslation(input){
+  
+  $.ajax({                    
+        type: "GET" , 
+        url: xmlCont,//Place that FULL_PATH here 
+        dataType: "xml" , //This too is important 
+  
+       success: function(xml) { 
+  
+        console.log(xml)// see the ajax results if your curious
+  
+      //var xmlDoc = $.parseXML( xml );//NOT NEED FOR THIS
+  
+      //console.log(xmlDoc);// if you parseXML it returns null
+  
+      var entries = $(xml).find('body').find('entry'); //Show the text between the <title> tags
+  
+      // var regex = new RegExp("^Europa$");
+      // speedy
+      // $(entries).filter(function () {
+      //     return regex.test($(this).text()); 
+      // });
+  
+      // for (i = 0; i < spanish.length; i++){
+      //    console.log(spanish[i].textContent)
+      // }
+      
+  
+       for (i = 0; i < entries.length; i++){
+          var entry = entries[i];
+          var spanish = entry.getElementsByTagName("form")[0].getElementsByTagName("orth")[0].textContent; 
+          var englishes = entry.getElementsByTagName("sense"); 
+          var english_list = []
+           for (j = 0; j < englishes.length; j++) {
+               var english = englishes[j].getElementsByTagName("cit")[0].getElementsByTagName("quote")[0].textContent;
+               english_list.push(english);
+           }
+  
+           if (spanish === input){
+               console.log(english_list);
+               return console.log(english_list.toString());
+           }
+       }
+  
+      //var entry = title.getElementsByTagName('entry').text(); //Show the text between the <title> tags
+  
+  }    
+      });
+  }
+  
+  
 //TODO: getTheMeaningOfTheWord should return the meaning of the word
 //hover should make a div that displays everything
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+    if (changeInfo.status == 'complete' && tab.active) {
+  
+      // do your things
+  
+    
 window.addEventListener("load", function() {
     let allPs = document.querySelectorAll("p");
     for (let i = 0; i < allPs.length; i++) {
@@ -37,3 +112,5 @@ function read(element) {
     myAudio.play();
 
 }
+}
+})
